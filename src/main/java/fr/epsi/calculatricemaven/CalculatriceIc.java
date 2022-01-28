55ee66f285e052787cfd3e5717fcac75858f8541
Administrator <admin@example.com>
2022-01-28 16:42:01 +0100

    Rules

---
+++ src/main/java/fr/epsi/calculatricemaven/CalculatriceIc.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fr.epsi.calculatricemaven;
+
+
+
+/**
+ *
+ * @author 33676
+ */
+public class CalculatriceIc {
+
+
+    /**
+     * 
+     * @param a
+     * @param b
+     * @return 
+     */
+    public double addition(double a, double b){
+        return a+b;
+    }
+    
+    /**
+     * fonction Soustraction, retourne le resultat de la soustraction 
+     * mathematique de a - b
+     * @param a de type entier
+     * @param b de type entier
+     * @return un entier 
+     */
+    public double soustraction(double a, double b){
+        return a-b;
+    }
+    
+    public double multiplication(double a, double b){
+        return a*b;
+    }
+    
+    
+    public double division(double a, double b) throws ExceptionByZero{
+        if(b == 0){
+            throw new ExceptionByZero("Division par zero");
+        }
+        return a/b;
+        
+    }
+}
